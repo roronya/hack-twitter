85a2c22161de2189feb4724786e629bed6109e4b
roronya <roronya628@gmail.com>
2023-08-18 22:43:27 +0900

    init

---
+++ index.js
@@ -0,0 +1,3 @@
+window.onclick = () => {
+  document.querySelector("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-zso239.r-1hycxz").innerHTML=""
+};
